facac86b5850a2c37770e237af0015fe94e00306
decidev22 <hamento22@gmail.com>
2023-04-28 00:07:59 +1200

    Listing display and favorite function per user

---
+++ dev-nomad/app/components/listings/ListingCard.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import { Listing, Reservation } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { format } from "date-fns";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
+
+interface ListingCard {
+  currentUser?: SafeUser | null;
+  reservation?: Reservation;
+  disabled?: boolean;
+  onAction?: (id: string) => void;
+  actionLabel?: string;
+  actionId?: string;
+  data: Listing;
+}
+
+const ListingCard: React.FC<ListingCard> = ({
+  reservation,
+  currentUser,
+  onAction,
+  actionId = "",
+  actionLabel,
+  data,
+  disabled,
+}) => {
+  const router = useRouter();
+  const { getByValue } = useCountries();
+  const location = getByValue(data.locationValue);
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+      if (disabled) {
+        return;
+      }
+      onAction?.(actionId);
+    },
+    [onAction, actionId, disabled]
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+    return data.price;
+  }, [reservation, data.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div
+      className="col-span-1 cursor-pointer group md:mt-2"
+      onClick={() => router.push("/listings/${data.id}")}
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image
+            alt="listing"
+            src={data.imageSrc}
+            className="object-cover h-full w-full group-hover:scale-110 transition"
+            fill
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton
+              listingId={data.id}
+              currentUser={currentUser}
+            />
+          </div>
+        </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row itmes-center gap-1">
+          $ {price}
+          <div className="">
+            {!reservation && <div className="font-light">/ day</div>}
+          </div>
+          {onAction && actionLabel && (
+            <Button
+              disabled={disabled}
+              small
+              label={actionLabel}
+              onClick={handleCancel}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListingCard;
